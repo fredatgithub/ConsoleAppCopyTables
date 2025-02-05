dcce7175a5e9b56ae96b9acee48864b814643e64
Fred <domaine75@gmail.com>
2025-02-05 09:20:38 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppCopyTablesInOrder/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Npgsql;
+
+namespace ConsoleAppCopyTablesInOrder
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      const string tableName = "activities";
+      var tablesToUpdate = GetUpdateOrder(tableName);
+      if (tablesToUpdate.Count == 0)
+      {
+        Console.WriteLine("No tables to update.");
+      }
+      else
+      {
+        Console.WriteLine($"There is {tablesToUpdate.Count} tables to be updated:");
+      }
+
+      foreach (var table in tablesToUpdate)
+      {
+        Console.WriteLine(table);
+      }
+
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private static string GettriggerTodisableSqlRequest()
+    {
+      return "SELECT 'ALTER TABLE ' || event_object_table || ' DISABLE TRIGGER ' || trigger_name || ';' from information_schema.triggers where trigger_schema = user ORDER by event_object_table, trigger_name;";
+    }
+
+    private List<string> GetTriggerListFromDatabase(string request, string connectionString)
+    {
+      var result = new List<string>();
+      using (var connection = new NpgsqlConnection(connectionString))
+      {
+        connection.Open();
+        using (var command = new NpgsqlCommand(request, connection))
+        {
+          using (var reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              result.Add(reader.GetString(0));
+            }
+          }
+        }
+      }
+
+      return result;
+    }
+
+    private static List<string> GetUpdateOrder(string tableName)
+    {
+      var connectionString = "Host=localhost;Username=username;Password=password;Database=database_name";
+      var tablesToUpdate = new List<string>();
+
+      using (var connection = new NpgsqlConnection(connectionString))
+      {
+        connection.Open();
+
+        string query = @"
+                WITH RECURSIVE dependency_tree AS (
+                    SELECT 
+                        tc.table_name, 
+                        ccu.table_name AS foreign_table_name
+                    FROM 
+                        information_schema.table_constraints AS tc
+                    JOIN 
+                        information_schema.key_column_usage AS kcu
+                    ON 
+                        tc.constraint_name = kcu.constraint_name
+                    JOIN 
+                        information_schema.constraint_column_usage AS ccu
+                    ON 
+                        ccu.constraint_name = tc.constraint_name
+                    WHERE 
+                        tc.constraint_type = 'FOREIGN KEY'
+                        AND tc.table_name = @TableName
+                    UNION
+                    SELECT 
+                        tc.table_name, 
+                        ccu.table_name AS foreign_table_name
+                    FROM 
+                        information_schema.table_constraints AS tc
+                    JOIN 
+                        information_schema.key_column_usage AS kcu
+                    ON 
+                        tc.constraint_name = kcu.constraint_name
+                    JOIN 
+                        information_schema.constraint_column_usage AS ccu
+                    ON 
+                        ccu.constraint_name = tc.constraint_name
+                    JOIN 
+                        dependency_tree dt
+                    ON 
+                        dt.table_name = tc.table_name
+                )
+                SELECT 
+                    DISTINCT table_name
+                FROM 
+                    dependency_tree
+                WHERE 
+                    table_name != @TableName
+                ORDER BY 
+                    table_name;
+            ";
+
+        using (var command = new NpgsqlCommand(query, connection))
+        {
+          command.Parameters.AddWithValue("@TableName", tableName);
+
+          using (var reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              tablesToUpdate.Add(reader.GetString(0));
+            }
+          }
+        }
+      }
+
+      return tablesToUpdate;
+    }
+  }
+}
---
+++ CopyTable/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using Npgsql;
+
+namespace CopyTable
+{
+  internal static class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      string connectionString = GetConnectionString();
+      // disable first all triggers
+      string sqlRequest = GetTriggerTodisableSqlRequest();
+      var triggerList = GetTriggerListFromDatabase(sqlRequest, connectionString);
+      bool disableTriggersOk = EnableOrDisableTriggers(connectionString, triggerList);
+      if (!disableTriggersOk)
+      {
+        Display("Error while disabling triggers.");
+        return;
+      }
+
+      // get prerequisite tables to be updated first
+      var tableName = "table1";
+      var tablesToUpdate = GetUpdateOrder(tableName, connectionString);
+      if (tablesToUpdate.Count == 0)
+      {
+        Display("No tables to update.");
+        
+      }
+      else
+      {
+        Display($"There is {tablesToUpdate.Count} tables to be updated:");
+      }
+
+      tablesToUpdate.Add(tableName);
+      foreach (var table in tablesToUpdate)
+      {
+        Display(table);
+      }
+
+      // Copy tables in order
+      foreach (var table in tablesToUpdate)
+      {
+        Display($"Copying table {table}...");
+        CopyTable(table, connectionString);
+      }
+
+      // enable triggers
+      Display("Enabling triggers...");
+      sqlRequest = GetTriggerToEnableSqlRequest();
+      triggerList = GetTriggerListFromDatabase(sqlRequest, connectionString);
+      bool enableTriggersOk = EnableOrDisableTriggers(connectionString, triggerList);
+      if (!enableTriggersOk)
+      {
+        Display("Error while enabling triggers.");
+      }
+
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private static void CopyTable(string table, string connectionString)
+    {
+      // to be implemented
+    }
+
+    private static List<string> GetUpdateOrder(string tableName, string connectionString)
+    {
+      var tablesToUpdate = new List<string>();
+      using (var connection = new NpgsqlConnection(connectionString))
+      {
+        connection.Open();
+
+        string query = @"
+                WITH RECURSIVE dependency_tree AS (
+                    SELECT 
+                        tc.table_name, 
+                        ccu.table_name AS foreign_table_name
+                    FROM 
+                        information_schema.table_constraints AS tc
+                    JOIN 
+                        information_schema.key_column_usage AS kcu
+                    ON 
+                        tc.constraint_name = kcu.constraint_name
+                    JOIN 
+                        information_schema.constraint_column_usage AS ccu
+                    ON 
+                        ccu.constraint_name = tc.constraint_name
+                    WHERE 
+                        tc.constraint_type = 'FOREIGN KEY'
+                        AND tc.table_name = @TableName
+                    UNION
+                    SELECT 
+                        tc.table_name, 
+                        ccu.table_name AS foreign_table_name
+                    FROM 
+                        information_schema.table_constraints AS tc
+                    JOIN 
+                        information_schema.key_column_usage AS kcu
+                    ON 
+                        tc.constraint_name = kcu.constraint_name
+                    JOIN 
+                        information_schema.constraint_column_usage AS ccu
+                    ON 
+                        ccu.constraint_name = tc.constraint_name
+                    JOIN 
+                        dependency_tree dt
+                    ON 
+                        dt.table_name = tc.table_name
+                )
+                SELECT 
+                    DISTINCT table_name
+                FROM 
+                    dependency_tree
+                WHERE 
+                    table_name != @TableName
+                ORDER BY 
+                    table_name;
+            ";
+
+        using (var command = new NpgsqlCommand(query, connection))
+        {
+          command.Parameters.AddWithValue("@TableName", tableName);
+
+          using (var reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              tablesToUpdate.Add(reader.GetString(0));
+            }
+          }
+        }
+      }
+
+      return tablesToUpdate;
+    }
+
+    private static bool EnableOrDisableTriggers(string connectionString, List<string> triggerList)
+    {
+      bool result = true;
+      try
+      {
+        using (var connection = new NpgsqlConnection(connectionString))
+        {
+          connection.Open();
+          foreach (var trigger in triggerList)
+          {
+            using (var command = new NpgsqlCommand(trigger, connection))
+            {
+              command.ExecuteNonQuery();
+            }
+          }
+        }
+      }
+      catch (Exception)
+      {
+        result = false;
+      }
+
+      return result;
+    }
+
+    private static string GetConnectionString()
+    {
+      return "Host=localhost;Username=toto;Password=titi;Database=tutu";
+    }
+
+    private static string GetTriggerTodisableSqlRequest()
+    {
+      return "SELECT 'ALTER TABLE ' || event_object_table || ' DISABLE TRIGGER ' || trigger_name || ';' from information_schema.triggers where trigger_schema = current_user;";
+    }
+
+    private static string GetTriggerToEnableSqlRequest()
+    {
+      return "SELECT 'ALTER TABLE ' || event_object_table || ' ENABLE TRIGGER ' || trigger_name || ';' from information_schema.triggers where trigger_schema = current_user;";
+    }
+
+    private static List<string> GetTriggerListFromDatabase(string sqlRequest, string connectionString)
+    {
+      var result = new List<string>();
+      using (var connection = new NpgsqlConnection(connectionString))
+      {
+        connection.Open();
+        using (var command = new NpgsqlCommand(sqlRequest, connection))
+        {
+          using (var reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              var triggerName = reader.GetString(0);
+              Console.WriteLine($"Found trigger: {triggerName}");
+              result.Add(triggerName);
+            }
+          }
+        }
+      }
+
+      return result;
+    }
+  }
+}
